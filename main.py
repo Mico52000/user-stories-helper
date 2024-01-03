c9f64b93296debdbd933ca65d85e08b7b8d5e162
Michael George Zakhary <61880954+Mico52000@users.noreply.github.com>
2024-01-03 22:09:15 +0200

    added button choices for which llm to use

--- main.py
+++ main.py
@@ -7,6 +7,32 @@ from streamlit_chat import message
 
 st.header("User Stories Helper Bot")
 
+
+# Define buttons
+mixtral_button = st.button("mixtral")
+gpt35_button = st.button("GPT 3.5 Turbo")
+gpt4_button = st.button("GPT 4")
+
+
+
+
+if 'selected_llm_option' not in st.session_state:
+    st.session_state['selected_llm_option'] = None
+if 'model_status_message' not in st.session_state:
+    st.session_state['model_status_message'] = 'Choose your model'
+
+if mixtral_button:
+    st.session_state['model_status_message'] = 'you are now chatting with  mixtral'
+    st.session_state['selected_llm_option'] = 1
+elif gpt35_button:
+    st.session_state['model_status_message'] = 'you are now chatting with gpt 3.5'
+    st.session_state['selected_llm_option'] = 2
+elif gpt4_button:
+    st.session_state['model_status_message'] = 'you are now chatting with gpt 4'
+    st.session_state['selected_llm_option'] = 3
+
+st.text(st.session_state['model_status_message'])
+
 prompt = st.text_input("prompt", placeholder="What are we building today?")
 
 if "user_prompt_history" not in st.session_state:
@@ -17,12 +43,19 @@ if "chat_history" not in st.session_state:
     st.session_state["chat_history"] = []
 
 
+
 if prompt:
     ## with notation allows for the spinner to dissapear after code inside is finished
     with st.spinner("Generating Response"):
+
+
+
+
         generated_response = run_llm(
-            query=prompt
+            query=prompt,
+            selected_llm_option= st.session_state['selected_llm_option']
         )
+
 
 
         formatted_response = (
@@ -34,9 +67,9 @@ if prompt:
         st.session_state["prompt"] = ""
 
 if st.session_state["bot_response_history"]:
-    for prompt, response in zip(
+    for i,(prompt, response) in enumerate(zip(
         st.session_state["user_prompt_history"],
         st.session_state["bot_response_history"],
-    ):
-        message(prompt, is_user=True)
-        message(response)
+    )):
+        message(prompt, is_user=True, key=f"user_message_{i}")
+        message(response, key=f"bot_message_{i}")
